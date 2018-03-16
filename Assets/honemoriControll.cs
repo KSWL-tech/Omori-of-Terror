431008e99ef85b03b0ca4102c7f514a50dd90208
gotutiyan <gto.rikujyo@icloud.com>
2018-03-16 18:22:44 +0900

    骨森くんを色々

---
+++ Assets/honemoriControll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class honemoriControll : MonoBehaviour {
+    public GameObject target;  //targetにはプレイヤーが入る。
+    public float diff=0,speed;
+    Vector3 pos, pastpos;
+    bool isNav = false;
+	// Use this for initialization
+	void Start () {
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = target.transform.position; //destinationには追従したいオブジェクトの座標が入る
+        pos = transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //NavMeshによる自動追従は常におこない、普段はLooakatによる移動を優先して行う。どこかにハマって動けなくなったとき、前フレームと比べた時の位置が非常に近くなるため、isNavをtrueにし、NavMeshによる追従のみにすることでこれを脱する。
+        transform.LookAt(target.transform);
+        if (!isNav)
+        {
+            transform.Translate(0, 0, speed);
+        }
+
+        pastpos = pos;
+        pos = transform.position;
+        if (Vector3.Distance(pos, pastpos) < diff)
+        {
+            isNav = true;
+        }
+        else isNav = false;
+    }
+}
