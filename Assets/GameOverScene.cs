67f60faf1e4afd0232e0a0a1f5d753a748d2ccce
nodoka8 <35965827+nodoka8@users.noreply.github.com>
2018-05-26 18:04:38 +0900

    音とかいろいろ

---
+++ Assets/GameOverScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScene : MonoBehaviour {
+
+    SceneController SceneController; 
+
+	// Use this for initialization
+	void Start () {
+        SceneController = GetComponent<SceneController>();
+
+        StartCoroutine("Change");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Change()
+    {
+        yield return new WaitForSeconds(6);
+
+        SceneController.changetitle();
+    }
+}
