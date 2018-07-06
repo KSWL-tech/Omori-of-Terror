443e740346e8b2045e7d681943084097eb603fb0
nodoka8 <35965827+nodoka8@users.noreply.github.com>
2018-07-06 23:56:50 +0900

    赤ちゃん実装できないよおおお

---
+++ Assets/Baby.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baby : MonoBehaviour {
+
+    public GameObject target;
+    bool isNav = false;
+    UnityEngine.AI.NavMeshAgent agent;
+    public float speed, diff;
+    public Vector3 pos, pastpos;
+    Quaternion def_rotation;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+        def_rotation = transform.rotation; //回転しないように。y軸周りの回転はLookAtでやってくれるので、x、z軸回転を固定にしようと思う
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.transform.position);
+
+    }
+}
