ed6a261125eb23c34fe984ca8ac3416624bc55ba
nodoka8 <35965827+nodoka8@users.noreply.github.com>
2018-12-24 17:06:16 +0900

    目

---
+++ Assets/Eye.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour {
+
+    Material mat;
+    private int pattern;
+    private float threshold;
+    public float speed;
+    private bool reflag = false;
+
+    // Use this for initialization
+    virtual protected void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+        StartCoroutine("BlinkUp");
+        pattern = (int)Random.Range(5, 20);
+	}
+
+    // Update is called once per frame
+    virtual protected void Update () {
+		if((int)Time.time % pattern == 0 && reflag)
+        {
+           // Debug.Log("Blink");
+            reflag = false;
+            StartCoroutine("BlinkDown");
+        }
+	}
+
+    IEnumerator BlinkDown()
+    {
+
+        yield return new WaitForFixedUpdate();
+
+        if(threshold <= 0)
+        {
+            StartCoroutine("BlinkUp");
+            yield break;
+        }
+
+        threshold -= speed;
+        mat.SetFloat("_Blink", threshold);
+        StartCoroutine("BlinkDown");
+    }
+
+    IEnumerator BlinkUp()
+    {
+
+        yield return new WaitForFixedUpdate();
+
+        if(threshold >= 0.5)
+        {
+           // Debug.Log("stop");
+            reflag = true;
+            yield break;
+        }
+
+        threshold += speed;
+        mat.SetFloat("_Blink", threshold);
+        StartCoroutine("BlinkUp");
+    }
+}
---
+++ Assets/EyeParent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeParent : Eye {
+
+    public bool flag;
+    public float nearspeed;
+
+    // Use this for initialization
+    override protected void Start () {
+        base.Start();
+	}
+
+    // Update is called once per frame
+    override protected void Update () {
+        base.Update();
+
+	}
+
+    private void FixedUpdate()
+    {
+        if (flag)
+        {
+            transform.Translate(0, 0, nearspeed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "EyeCol")
+        {
+            Debug.Log("目線が合う～");
+            flag = true;
+        }
+    }
+
+}
