3af63d706000dad8ebaa5934277a645f1b1db3f8
nodoka8 <35965827+nodoka8@users.noreply.github.com>
2018-12-25 15:57:35 +0900

    トンネルギミック完成

---
+++ Assets/SlideX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideX : MonoBehaviour {
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("FPSController");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
