998e775244a17c3ebf5e0bb66c1ed697978dd6a7
gem install bundler <36186204+Mayutotsu@users.noreply.github.com>
2019-01-01 02:20:29 +0900

    単眼森くんのスクリプトを書き始めました

---
+++ Assets/Scene/shinji/Tanganmori/Tanganmori.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tanganmori : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator animator;
+	public GameObject pointsParent;
+	GameObject[] walkPoints;
+	public GameObject nearestObject;//最も近いオブジェクト
+	bool isTrace;
+	void Start () {
+		float dis=Mathf.Infinity;
+		Transform[] childTransform = pointsParent.GetComponentsInChildren<Transform>();
+		walkPoints=new GameObject[childTransform.Length];
+		for(int i=0;i<walkPoints.Length;i++){
+			walkPoints[i]=childTransform[i].gameObject;
+			if( Vector3.Distance(transform.position,walkPoints[i].transform.position)<dis){
+				dis=Vector3.Distance(transform.position,walkPoints[i].transform.position);
+				nearestObject=walkPoints[i];
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
