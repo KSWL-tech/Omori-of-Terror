702ad0d6331a455ae7e507f7cf9661099725d459
nodoka8 <35965827+nodoka8@users.noreply.github.com>
2018-12-31 17:49:23 +0900

    洞窟敵

--- Assets/Baby.cs
+++ Assets/Baby.cs
@@ -1,26 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Baby : MonoBehaviour {
 
     public GameObject target;
     bool isNav = false;
-    UnityEngine.AI.NavMeshAgent agent;
+    NavMeshAgent agent;
     public float speed, diff;
     public Vector3 pos, pastpos;
     Quaternion def_rotation;
 
     // Use this for initialization
     void Start () {
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.SetDestination(target.transform.position);
-        def_rotation = transform.rotation; //回転しないように。y軸周りの回転はLookAtでやってくれるので、x、z軸回転を固定にしようと思う
+        agent = GetComponent<NavMeshAgent>();
+       // agent.destination = target.transform.position;
+       // def_rotation = transform.rotation; //回転しないように。y軸周りの回転はLookAtでやってくれるので、x、z軸回転を固定にしようと思う
     }
 	
 	// Update is called once per frame
 	void Update () {
-        agent.SetDestination(target.transform.position);
+        if (agent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            //navMeshAgentの操作
+            Debug.Log("Navigate");
+            agent.destination = target.transform.position;
+
+        }
 
     }
 }
---
+++ Assets/ExitAppear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitAppear : MonoBehaviour {
+
+    public GameObject falseRoad;
+    public GameObject exitRoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            falseRoad.SetActive(false);
+            exitRoad.SetActive(true);
+        }
+    }
+}
